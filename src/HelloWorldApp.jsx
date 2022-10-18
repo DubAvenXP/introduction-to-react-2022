6b77038553217eb1c4906935f0807826b5f1a504
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-10-17 22:09:41 -0600

    feat: first component

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,11 @@
+// /* export */ function App() {
+//     return (<h1>Hello World</h1>);
+// }
+
+// export default App;
+
+export const HelloWorldApp = () => {
+    return <h1>HelloWorldApp</h1>;
+};
+
+// export const HelloWorldApp = () => <h1>HelloWorldApp</h1>;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-function App() {
-    return <h1>Hello World</h1>;
-}
+import { HelloWorldApp } from "./HelloWorldApp";
+import { FirstApp } from "./FirstApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <HelloWorldApp />
+        <FirstApp />
     </React.StrictMode>
 );
