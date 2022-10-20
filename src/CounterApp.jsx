f9d048351e7f65133929932e6d4c547e0349d99c
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-10-20 13:21:39 -0600

    feat: first hook

---
+++ src/CounterApp.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+import PropTypes from "prop-types";
+
+// If the handleAdd function not use any prop, we can define it outside the component
+// const handleAdd = (event, newValue) => {
+//     console.log("Hi");
+// }
+
+
+// Functional component
+export const CounterApp = ({ value }) => {
+
+    // When the state changes, the component will be re-rendered with the new value
+    const [counter, setCounter] = useState(value);
+
+    const handleAdd = () => {
+        setCounter(counter + 1);
+        // setCounter((c) => c + 2);
+    };
+
+    const handleSubtract = () => {
+        setCounter(counter - 1);
+    };
+
+    const handleReset = () => {
+        setCounter(value);
+    };
+    return (
+        <>
+            <h1>CounterApp</h1>
+            {/* <button onClick={ (event) => handleAdd(event, "hi") }> -1 </button> */}
+
+            <button onClick={handleSubtract}> -1 </button>
+            <span> {counter} </span>
+            <button onClick={handleAdd}> +1 </button>
+
+            <button onClick={handleReset}> Reset </button>
+        </>
+    );
+};
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { HelloWorldApp } from "./HelloWorldApp";
-import { FirstApp } from "./FirstApp";
+// import { HelloWorldApp } from "./HelloWorldApp";
+// import { FirstApp } from "./FirstApp";
+import { CounterApp } from "./CounterApp";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     // React.StrictMode is a component that wraps the app and checks for potential problems
     <React.StrictMode>
-        <HelloWorldApp />
-        <FirstApp title="Hola, soy Goku" subtitle={123} />
+        {/* <HelloWorldApp />
+        <FirstApp title="Hola, soy Goku" subtitle={123} /> */}
+        <CounterApp value={10}/>
     </React.StrictMode>
 );
